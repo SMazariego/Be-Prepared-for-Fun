68318b1d1bc4c999d2851335378bbaa0e3659d44
CharissaHollister <charissa.hollister@diasorin.com>
2022-07-19 10:32:39 -0500

    update controller index files with correct variables

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,15 @@
 const router = require("express").Router();
 
 const userRoutes = require("./user-routes.js");
-const postRoutes = require("./post-routes");
-const commentRoutes = require("./comment-routes");
+const vacayRoutes = require("./vacay-routes");
+const agendaRoutes = require("./agenda-item-routes");
+const packingListRoutes = require("./packing-list-routes");
+const packingItemRoutes = require("./packing-item-routes");
 
 router.use("/users", userRoutes);
-router.use("/posts", postRoutes);
-router.use("/comments", commentRoutes);
+router.use("/vacay", vacayRoutes);
+router.use("/agenda", agendaRoutes);
+router.use("/packing", packingListRoutes);
+// router.use("/vacay", packingItemRoutes);
 
 module.exports = router;
--- controllers/api/vacay-routes.js
+++ controllers/api/vacay-routes.js
@@ -12,12 +12,6 @@ router.get("/", (req, res) => {
       "post_url",
       "title",
       "created_at",
-      [
-        sequelize.literal(
-          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
-        ),
-        "vote_count",
-      ],
     ],
     include: [
       {
@@ -51,12 +45,6 @@ router.get("/:id", (req, res) => {
       "post_url",
       "title",
       "created_at",
-      [
-        sequelize.literal(
-          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
-        ),
-        "vote_count",
-      ],
     ],
     include: [
       {
