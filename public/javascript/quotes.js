3988f0489718cab2bef1ece17db2ffad905a0864
CharissaHollister <charissa.hollister@diasorin.com>
2022-07-19 20:29:33 -0500

    quote api drafted

--- public/javascript/quotes.js
+++ public/javascript/quotes.js
@@ -1,9 +1,9 @@
 // https://zenquotes.io/api/random?option1=value&option2=value
 // Random quote generator api
 
+const apiURL = "https://zenquotes.io/api/quotes";
 
-const api_url = "https://zenquotes.io/api/random";
-
+//todo need to fix cors error
 async function getapi(url) {
   //get a random number to pick one of the quotes
   let c = Math.floor(Math.random() * 30);
@@ -13,10 +13,10 @@ async function getapi(url) {
   document.getElementById("quoteArea").textContent = quoteData[c].q;
 }
 
-getapi(api_url);
+getapi(apiURL);
+
 
 ////////////////////////////
-
 
 //! add to footer as attribution
 // Inspirational quotes provided by <a href="https://zenquotes.io/" target="_blank">ZenQuotes API</a>
