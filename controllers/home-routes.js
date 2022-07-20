44971b32f2ae747ef3f9393ad6ceed692efec7d2
CharissaHollister <charissa.hollister@diasorin.com>
2022-07-20 16:10:57 -0500

    redirect to dashboard if already logged in

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
-const sequelize = require("../config/connection");
-// const {User} = require("../models");
+
 
 //?do we reference the quotes api here for it to populate on the homepage?
 // get all posts for homepage
@@ -9,16 +8,11 @@ router.get("/", (req, res) => {
       res.render("homepage", {
         loggedIn: req.session.loggedIn,
       });
+      if (req.session.loggedIn) {
+        res.redirect("/dashboard");
+        return;
+      }
 });
 
-
-router.get("/login", (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect("/dashboard");
-    return;
-  }
-
-  res.render("login");
-});
 
 module.exports = router;
