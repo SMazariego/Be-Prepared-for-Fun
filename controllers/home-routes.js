13c29cbac1319c296010fececee026d5c6c93d5d
CharissaHollister <charissa.hollister@diasorin.com>
2022-07-20 13:50:32 -0500

    fix homeroutes to not error

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,16 @@
 const router = require("express").Router();
-// const sequelize = require("../config/connection");
+const sequelize = require("../config/connection");
 // const {User} = require("../models");
 
 //?do we reference the quotes api here for it to populate on the homepage?
+// get all posts for homepage
+router.get("/", (req, res) => {
+  console.log("======================");
+      res.render("homepage", {
+        loggedIn: req.session.loggedIn,
+      });
+});
+
 
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
