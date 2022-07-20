653f1f6e61270fdc1519be722111ae4c2d1000fb
CharissaHollister <charissa.hollister@diasorin.com>
2022-07-20 17:17:25 -0500

    working on vacay handlebars and routes

--- public/javascript/agenda.js
+++ public/javascript/agenda.js
@@ -1,6 +1,6 @@
-function test() {
+function addAgendaHandler() {
   document.getElementById("quoteArea").textContent =
     "Quote will populate from external api here";
 }
 
-document.querySelector(".update-agenda-btn").addEventListener("click", test);
+document.querySelector(".add-agenda-btn").addEventListener("click", addAgendaHandler);
