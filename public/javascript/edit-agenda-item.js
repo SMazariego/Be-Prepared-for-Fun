7a447065721ff76bfa053925842e305508d571fd
CharissaHollister <charissa.hollister@diasorin.com>
2022-07-21 10:44:36 -0500

    add agenda form to vm to edit vacay handlebar

--- public/javascript/edit-agenda-item.js
+++ public/javascript/edit-agenda-item.js
@@ -1,1 +1,6 @@
 //todo figure out how to make work for calendar style
+function test() {
+  console.log("event edit area");
+}
+
+document.querySelector(".update-agenda-btn").addEventListener("click", test);
