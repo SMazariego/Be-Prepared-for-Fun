3fd5b6ef53aa60388b663810755df791e0207624
CharissaHollister <charissa.hollister@diasorin.com>
2022-07-22 12:39:26 -0500

    fix typo

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const exphbs = require("express-handlebars");
 //const Agenda = require("agenda"); //?belongs here or another js?
 
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
