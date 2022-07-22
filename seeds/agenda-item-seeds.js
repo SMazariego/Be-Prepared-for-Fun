438f97fe7d342e302f0e5465784d8a83c2980434
CharissaHollister <charissa.hollister@diasorin.com>
2022-07-22 11:45:52 -0500

    add model fields to seed files

--- seeds/agenda-item-seeds.js
+++ seeds/agenda-item-seeds.js
@@ -2,12 +2,41 @@ const { AgendaItem } = require("../models");
 
 const agendadata = [
   {
-    agenda_text:
-      "Eleven Earths could fit across Jupiter’s equator. If Earth were the size of a grape, Jupiter would be the size of a basketball.",
-    user_id: 2,
+    agenda_title: "flight",
+    agenda_date: "2022-11-05",
+    agenda_start: "06:32:00",
+    agenda_end: "14:55:00",
+    location: "the sky",
+    agenda_notes: "Sue will join on a later flight",
     vacay_id: 1,
   },
-
+  {
+    agenda_title: "dinner",
+    agenda_date: "2022-11-08",
+    agenda_start: "16:10:00",
+    agenda_end: "22:00:00",
+    location: "The Luau Grill",
+    agenda_notes: "luau show followed by buffet dinner. Bobby has the tickets",
+    vacay_id: 1,
+  },
+  {
+    agenda_title: "beach picnic",
+    agenda_date: "2022-11-11",
+    agenda_start: "10:00:00",
+    agenda_end: "14:00:00",
+    location: "Kaanapali beach",
+    agenda_notes: "Pot Luck",
+    vacay_id: 1,
+  },
+  {
+    agenda_title: "leaving 😭",
+    agenda_date: "2022-11-13",
+    agenda_start: "16:32:00",
+    agenda_end: "23:59:00",
+    location: "the sky",
+    agenda_notes: "Dan picking us up at msp",
+    vacay_id: 1,
+  },
 ];
 
 const seedAgendaItems = () => AgendaItem.bulkCreate(agendadata);
--- seeds/vacay-seeds.js
+++ seeds/vacay-seeds.js
@@ -4,8 +4,10 @@ const vacaydata = [
   {
     title: "Test Vacation 1",
     user_id: 1,
+    start_date: "2022-11-05",
+    end_date: "2022-11-13",
+    destination: "Hawaii",
   },
-  
 ];
 
 const seedVacays = () => Vacay.bulkCreate(vacaydata);
