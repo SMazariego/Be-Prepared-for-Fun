23ae5ad4e8c61c3afcbd85a020bd6021b4864d83
CharissaHollister <charissa.hollister@diasorin.com>
2022-07-22 11:25:46 -0500

    timeto merge

--- public/javascript/delete-agenda-item.js
+++ public/javascript/delete-agenda-item.js
@@ -3,6 +3,8 @@ async function deleteAgendaFormHandler(event) {
   event.preventDefault();
   console.log("delete started");
   const id = event.target.id;
+
+  //todo figure out how to target id of agenda-item selected
   console.log(id);
   const response = await fetch(`/api/agendas/${id}`, {
     method: "DELETE",
