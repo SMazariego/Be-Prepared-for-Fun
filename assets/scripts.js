72488dd7f676d5bcbace5b640f2e7883d4beea95
SMazariego <102334091+SMazariego@users.noreply.github.com>
2022-07-22 22:42:11 -0400

    Continued Progress On Packing/inclusion of assets and script.js

---
+++ assets/scripts.js
@@ -0,0 +1,89 @@
+
+let existValue = "";
+$('input:checkbox').click(function(){
+    var tb = "#"+$(this).attr('rel');
+    let text_to_add = this.name + "\n";
+
+    var checkName = this.name;
+    var checked = this.checked;
+
+    localStorage.setItem(checkName,checked);
+
+
+    let inputVal = $('#pname').val()
+    //when click a checkbox and show checked items in the text area
+    if($(this).is(":checked")){
+        $(tb).val($(tb).val() + text_to_add);
+    }else{
+        let remove = this.name +"\n";
+        //when a box is unchecked it clears the previously populated text from checkbox
+        $(tb).val($(tb).val().replace(remove,""));
+    }
+
+    //storing the value to existValue
+
+    existValue = $(tb).val().replace(`${inputVal}\n`, "");
+
+    refresh();
+        
+});
+
+$('#pname').on('input',(e)=>{
+    //here to adding the input value to the existValue
+    $('#textbox1').val(`${e.target.value}\n${existValue}`);
+
+    localStorage.setItem('pname',$('#pname').val());
+    refresh();
+});
+
+//modal triggered
+$('#submit-temp').click(function (event) {
+    $('#exl').modal({
+        fadeDuration: 250
+    
+    });
+    $('input[name=subject]').val($('input[name=pname]').val());
+    let selectedValues = $('input:checkbox:checked').map(function () {
+        return this.name;
+    }).get().join(",");
+    $('textarea[name=mail-body]').val(selectedValues);
+
+
+    return false;
+});
+
+//email sending
+$('#mail-sending').click(function () {
+    let to = $('input[name=mailto]');
+    let subject = $('input[name=subject]').val();
+    let body = $('textarea[name=mail-body]').val();
+    //window.open('mailto:'+to+'?subject='+encodeURIComponent(subject)+'&body='+encodeURIComponent(body));
+
+    emailjs.send("service_swqmzcu","template_1hb9zlf",{
+        subject: subject,
+        body: body,
+    }).then(function(response) {
+        console.log('SUCCESS!', response.status, response.text);
+        alert("Send success\nresponse code is " + response.status);
+    }, function(error) {
+        console.log('FAILED...', error);
+        alert("Send failed\nresponse code is " + error);
+    });
+
+
+});
+
+
+function refresh(){
+
+    $('input[name=subject]').val($('input[name=pname]').val());
+    let selectedValues = $('input:checkbox:checked').map(function () {
+        return this.name;
+    }).get().join(",");
+    $('textarea[name=mail-body]').val(selectedValues);
+
+    var need2fill = 'project: ' + $('input[name=pname]').val();
+    need2fill += '\nselectedValues: '+selectedValues;
+    $('#textbox1').val(need2fill);
+}
+
