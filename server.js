0f50cea889788bf094a1b9c8e09ec27b86c0dd4e
Charissa Hollister <charissa.hollister@diasorin.com>
2022-07-26 16:04:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ app.use(require("./controllers"));
 
 //! change to force:false for launch
 
-sequelize.sync({ force: true}).then(() => {
+sequelize.sync({ force: false}).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
